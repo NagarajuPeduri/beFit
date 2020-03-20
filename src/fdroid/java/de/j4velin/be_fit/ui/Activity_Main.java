7f729c61fb24590bd0374cf667af160a8b9f109b
nagarajuPeduri <pedurinagaraju7@gmail.com>
2020-03-20 11:14:35 +0530

    added befit application

---
+++ src/fdroid/java/de/j4velin/be_fit/ui/Activity_Main.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright 2013 Thomas Hoffmann
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.j4velin.be_fit.ui;
+
+import android.Manifest;
+import android.app.Fragment;
+import android.app.FragmentTransaction;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.content.PermissionChecker;
+import android.view.MenuItem;
+
+import de.j4velin.be_fit.BuildConfig;
+import de.j4velin.be_fit.R;
+import de.j4velin.be_fit.SensorListener;
+
+public class Activity_Main extends FragmentActivity {
+
+    @Override
+    protected void onCreate(final Bundle b) {
+        super.onCreate(b);
+        startService(new Intent(this, SensorListener.class));
+        if (b == null) {
+            // Create new fragment and transaction
+            Fragment newFragment = new Fragment_Overview();
+            FragmentTransaction transaction = getFragmentManager().beginTransaction();
+
+            // Replace whatever is in the fragment_container view with this
+            // fragment,
+            // and add the transaction to the back stack
+            transaction.replace(android.R.id.content, newFragment);
+
+            // Commit the transaction
+            transaction.commit();
+        }
+
+        if (BuildConfig.DEBUG && Build.VERSION.SDK_INT >= 23 && PermissionChecker
+                .checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) !=
+                PermissionChecker.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 0);
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (getFragmentManager().getBackStackEntryCount() > 0) {
+            getFragmentManager().popBackStackImmediate();
+        } else {
+            finish();
+        }
+    }
+
+    public boolean optionsItemSelected(final MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                getFragmentManager().popBackStackImmediate();
+                break;
+            case R.id.action_settings:
+                getFragmentManager().beginTransaction()
+                        .replace(android.R.id.content, new Fragment_Settings()).addToBackStack(null)
+                        .commit();
+                break;
+
+        }
+        return true;
+    }
+}
